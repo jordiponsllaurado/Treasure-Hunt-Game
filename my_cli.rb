61c091ffdd5e725a862a15757e4be812897a3a52
jordi.pons <jordi.pons@typeform.com>
2017-02-17 12:41:35 +0100

    Added basic game implementation using sequel

--- my_cli.rb
+++ my_cli.rb
@@ -1,24 +1,66 @@
-require "thor"
+require 'thor'
+require 'terminal-table'
+require 'sequel'
+require 'sqlite3'
 
 class MyCLI < Thor
-  desc "hello NAME", "say hello to NAME"
-  def hello(name)
-    puts "Hello #{name}"
-  end
 
-  desc "play", "say hello to NAME"
+  DB = Sequel.sqlite('treasure.db')
+
+  desc 'play', 'Creates a new map'
   def play
-    # TODO
+    treasure_x = rand(10)
+    treasure_y = rand(10)
+
+    unless DB.table_exists? :map
+      DB.create_table(:map) do
+        primary_key :id
+        String :name
+        Integer :pos_x
+        Integer :pos_y
+      end
+    end
+
+    ds = DB[:map]
+    if ds.where(:name => 'treasure').first
+      ds.where(:name => 'treasure').update(:pos_x => treasure_x, :pos_y => treasure_y)
+    else
+      ds.insert(:name => 'treasure', :pos_x => treasure_x, :pos_y => treasure_y)
+    end
   end
 
-  desc "stop", "say hello to NAME"
+  desc 'stop', 'say hello to NAME'
   def stop
     # TODO
   end
 
-  desc "check position X Y", "say if the treasure is there"
-  def check_position(x, y)
-    # TODO
+  desc 'position X Y', 'say if the treasure is there'
+  def position(x, y)
+    if !x.to_i.between?(0, 10) || !y.to_i.between?(0, 10)
+      puts 'Coordinates out of the map!'
+    else
+      ds = DB[:map]
+      result = ds.where(:name => 'treasure')
+      treasure_x = result.first[:pos_x]
+      treasure_y =  result.first[:pos_y]
+      zone_x =  (x.to_i - treasure_x).abs
+      zone_y = (y.to_i - treasure_y).abs
+      print_results(zone_x, zone_y)
+    end
+  end
+
+  private
+
+  def print_results(zone_x, zone_y)
+    if zone_x.eql?(0) && zone_y.eql?(0)
+      puts 'SUCESS!!!!!!!!!!!!!!'
+    elsif zone_x < 2 && zone_y < 2
+      puts 'HOT HOT HOT!!!'
+    elsif zone_x < 3 && zone_y < 3
+      puts 'WARM!'
+    else
+      puts 'COLD!!!'
+    end
   end
 end
 
